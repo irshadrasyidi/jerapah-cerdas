513e63f89e8ac9ac4130c504721c73cdb3e4c0d4
irshadrasyidi <didot.brodot@yahoo.com>
2018-09-30 00:12:27 +0700

    hrsnya bisa

--- app.py
+++ app.py
@@ -64,8 +64,8 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='oposeduh'))
     if text=="tyo":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
-    original_content_url='https://ichef.bbci.co.uk/news/1024/branded_indonesia/9224/production/_92421473_kecoa2.jpg',
-    preview_image_url='https://ichef.bbci.co.uk/news/1024/branded_indonesia/9224/production/_92421473_kecoa2.jpg'
+    original_content_url='http://thedotbruh.fun/kamus/1501901141247.jpg',
+    preview_image_url='http://thedotbruh.fun/kamus/1501901141247.jpg'
     ))
 
     if text=="bego":
