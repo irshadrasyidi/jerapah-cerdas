462e05d7e32494f2825a3fba92bf7bfbe9cae948
irshadrasyidi <didot.brodot@yahoo.com>
2018-10-20 12:46:57 +0700

    yopose

--- app.py
+++ app.py
@@ -155,6 +155,18 @@ def handle_message(event):
     sender = event.source.user_id #get usesenderr_id
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
+    
+    data=text.split('-')
+    if(data[0]=='/tambah'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=inputHewan(data[1], data[2], data[3], data[4], data[5])))
+    elif(data[0]=='/lihat'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=cariHewan(data[1])))
+    elif(data[0]=='/hapus'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=delHewan(data[1])))
+    elif(data[0]=='/koleksi-hewan'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=showAll()))
+    # elif(data[0]=='/ganti'):
+    #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=updateHewan(data[1],data[2],data[3],data[4],data[5],data[6])))
 
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=menu))
     if text=="/menu":
@@ -205,19 +217,6 @@ def handle_message(event):
                 ]
             )
         ))
-        
-    data=text.split('-')
-    if(data[0]=='/tambah'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=inputHewan(data[1], data[2], data[3], data[4], data[5])))
-    elif(data[0]=='/lihat'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=cariHewan(data[1])))
-    elif(data[0]=='/hapus'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=delHewan(data[1])))
-    elif(data[0]=='/koleksi-hewan'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=showAll()))
-    # elif(data[0]=='/ganti'):
-    #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=updateHewan(data[1],data[2],data[3],data[4],data[5],data[6])))
-
     if text=="/koleksi-meme":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='kamus shitpost :\n'))
     if text=="vivat":
