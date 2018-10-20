c4f394d002d06c51a85af856029cffd66f345b95
irshadrasyidi <didot.brodot@yahoo.com>
2018-10-20 17:20:52 +0700

    wer

--- app.py
+++ app.py
@@ -237,30 +237,30 @@ def handle_message(event):
                             data='action=buy&itemid=2'
                         )
                     ),
-                    ImageCarouselColumn(
-                        image_url='https://3.bp.blogspot.com/-3DCApIDpPg8/WadDwhqKGlI/AAAAAAAALVQ/cLo2R5M3C3AHmdC7WH7YZr-q_mwYu_0BACLcBGAs/s1600/kak-seto.jpg',
-                        action=PostbackAction(
-                            label='Kak Seto',
-                            text='/meme-seto',
-                            data='action=buy&itemid=3'
-                        )
-                    ),
-                    ImageCarouselColumn(
-                        image_url='https://s.kaskus.id/images/2018/03/12/7034635_201803120552320355.jpg',
-                        action=PostbackAction(
-                            label='WikiHow',
-                            text='/meme-wikihow',
-                            data='action=buy&itemid=4'
-                        )
-                    ),
-                    ImageCarouselColumn(
-                        image_url='http://tps2u.com/wp-content/uploads/2017/11/OTHERS-1.jpg',
-                        action=PostbackAction(
-                            label='Others',
-                            text='/meme-others',
-                            data='action=buy&itemid=5'
-                        )
-                    )
+                    # ImageCarouselColumn(
+                    #     image_url='https://3.bp.blogspot.com/-3DCApIDpPg8/WadDwhqKGlI/AAAAAAAALVQ/cLo2R5M3C3AHmdC7WH7YZr-q_mwYu_0BACLcBGAs/s1600/kak-seto.jpg',
+                    #     action=PostbackAction(
+                    #         label='Kak Seto',
+                    #         text='/meme-seto',
+                    #         data='action=buy&itemid=3'
+                    #     )
+                    # ),
+                    # ImageCarouselColumn(
+                    #     image_url='https://s.kaskus.id/images/2018/03/12/7034635_201803120552320355.jpg',
+                    #     action=PostbackAction(
+                    #         label='WikiHow',
+                    #         text='/meme-wikihow',
+                    #         data='action=buy&itemid=4'
+                    #     )
+                    # ),
+                    # ImageCarouselColumn(
+                    #     image_url='http://tps2u.com/wp-content/uploads/2017/11/OTHERS-1.jpg',
+                    #     action=PostbackAction(
+                    #         label='Others',
+                    #         text='/meme-others',
+                    #         data='action=buy&itemid=5'
+                    #     )
+                    # )
                 ]
             )
         ))
