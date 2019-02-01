55e048337f15ef0d3e9d5cdd80cd41ff71cb5d30
irshadrasyidi <didot.brodot@yahoo.com>
2019-02-01 23:01:59 +0700

    komit bnyk

--- app.py
+++ app.py
@@ -160,9 +160,19 @@ def handle_message(event):
 #MULT CHOICE
     #BAWEL
     #ANJING 8, PUN 2
-    if data[0]=="bawel":
+    if data[0]=="bawel" or data[1]=="bawel":
         a = random.randint(0, 1)
         b = ["https://s.kaskus.id/images/2018/03/14/7034635_201803140545070870.jpg", "https://1.bp.blogspot.com/-AHXu8nVDgww/Wr3YdzZvCVI/AAAAAAAABPg/AO7Mnycr_oQx3bdzboe6qyW8WyqjynfXACLcBGAs/s1600/IMG_20180327_222109.jpg"]
+        line_bot_api.reply_message(event.reply_token,ImageSendMessage(
+    original_content_url=b[a],
+    preview_image_url=b[a]
+    ))
+
+    #BODOH
+    #WIKIHOW 9, WIKIHOW 10
+    if data[0]=="bodoh" or data[1]=="bodoh" or data[0]=="bodo" or data[1]=="bodo":
+        a = random.randint(0, 1)
+        b = ["https://ktawa.com/wp-content/uploads/2018/05/2761997_201803120735020920.jpg", "https://ktawa.com/wp-content/uploads/2018/09/DXgqnNRVwAA-_6F.jpg"]
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url=b[a],
     preview_image_url=b[a]
@@ -248,115 +258,115 @@ def handle_message(event):
     preview_image_url='https://1.bp.blogspot.com/-AHXu8nVDgww/Wr3YdzZvCVI/AAAAAAAABPg/AO7Mnycr_oQx3bdzboe6qyW8WyqjynfXACLcBGAs/s1600/IMG_20180327_222109.jpg'
     ))
     #3 MULT CHOICE
-    if text=="/pun-bego" or data[0]=="bego":
+    if text=="/pun-bego" or data[0]=="bego" or data[1]=="bego":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DYaIxpdU0AE2UcO.jpg',
     preview_image_url='https://pbs.twimg.com/media/DYaIxpdU0AE2UcO.jpg'
     ))
     #4 MULT CHOICE
-    if text=="/pun-ngegas" or data[0]=="ngegas":
+    if text=="/pun-ngegas" or data[0]=="ngegas" or data[1]=="ngegas":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://s.kaskus.id/images/2017/10/07/9824467_201710070650410306.jpg',
     preview_image_url='https://s.kaskus.id/images/2017/10/07/9824467_201710070650410306.jpg'
     ))
     #5 MULT CHOICE
-    if text=="/pun-gas" or data[0]=="gas":
+    if text=="/pun-gas" or data[0]=="gas" or data[1]=="gas":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://s.kaskus.id/images/2017/05/01/9437462_201705010152380796.jpg',
     preview_image_url='https://s.kaskus.id/images/2017/05/01/9437462_201705010152380796.jpg'
     ))
     #6
-    if text=="/pun-gawat" or data[0]=="gawat":
+    if text=="/pun-gawat" or data[0]=="gawat" or data[1]=="gawat":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://em.wattpad.com/a597dfd0d5b0115078e72028c89b42f412c398e2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f4c346e547651464b426c66765a513d3d2d3532352e313531613331396163343031306232343335303035353539383537312e6a7067?s=fit&w=720&h=720',
     preview_image_url='https://em.wattpad.com/a597dfd0d5b0115078e72028c89b42f412c398e2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f4c346e547651464b426c66765a513d3d2d3532352e313531613331396163343031306232343335303035353539383537312e6a7067?s=fit&w=720&h=720'
     ))
     #6
-    if text=="/pun-gelut" or data[0]=="gelut":
+    if text=="/pun-gelut" or data[0]=="gelut" or data[1]=="gelut":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pics.me.me/gelut-29735709.png',
     preview_image_url='https://pics.me.me/gelut-29735709.png'
     ))
     #7 MULT CHOICE
-    if text=="/pun-ikan-goblok" or data[0]=="goblok":
+    if text=="/pun-ikan-goblok" or data[0]=="goblok" or data[1]=="goblok" or data[0]=="gblk" or data[1]=="gblk" or data[0]=="goblog" or data[1]=="goblog" or data[0]=="gblg" or data[1]=="gblg":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://i.pinimg.com/236x/d2/55/46/d25546911ab75e65de5d0e5b6f267c5c.jpg',
     preview_image_url='https://i.pinimg.com/236x/d2/55/46/d25546911ab75e65de5d0e5b6f267c5c.jpg'
     ))
     #8
-    if text=="/pun-bubar" or data[0]=="bubar" or data[0]=="leren":
+    if text=="/pun-bubar" or data[0]=="bubar" or data[0]=="leren" or data[1]=="bubar" or data[1]=="leren":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DY9OLOLVwAEadK7.jpg',
     preview_image_url='https://pbs.twimg.com/media/DY9OLOLVwAEadK7.jpg'
     ))
     #9
-    if text=="/pun-kancil" or data[0]=="kancil":
+    if text=="/pun-kancil" or data[0]=="kancil" or data[1]=="kancil":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DjSIfjUVAAAX5WQ.jpg:large',
     preview_image_url='https://pbs.twimg.com/media/DjSIfjUVAAAX5WQ.jpg:large'
     ))
     #10
-    if text=="/pun-kecewa" or data[0]=="kecewa":
+    if text=="/pun-kecewa" or data[0]=="kecewa" or data[1]=="kecewa":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pics.me.me/kecewa-31619297.png',
     preview_image_url='https://pics.me.me/kecewa-31619297.png'
     ))
     #11
-    if text=="/pun-keren" or data[0]=="keren" or data[0]=="cool" or data[0]=="kewl":
+    if text=="/pun-keren" or data[0]=="keren" or data[0]=="cool" or data[0]=="kewl" or data[1]=="keren" or data[1]=="cool" or data[1]=="kewl":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://media.keepo.me/20180306164724/511x288--28872329_1460633664058488_1580595060448431199_n.jpg',
     preview_image_url='https://media.keepo.me/20180306164724/511x288--28872329_1460633664058488_1580595060448431199_n.jpg'
     ))
     #12
-    if text=="/pun-kocak" or data[0]=="kocak" or data[0]=="ngakak":
+    if text=="/pun-kocak" or data[0]=="kocak" or data[0]=="ngakak" or data[1]=="kocak" or data[1]=="ngakak":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DYL2-5dVoAARtb4.jpg',
     preview_image_url='https://pbs.twimg.com/media/DYL2-5dVoAARtb4.jpg'
     ))
     #13
-    if text=="/pun-lodeh" or data[0]=="terserah" or data[0]=="serah" or data[0]=="seterah":
+    if text=="/pun-lodeh" or data[0]=="terserah" or data[0]=="serah" or data[0]=="seterah" or data[1]=="terserah" or data[1]=="serah" or data[1]=="seterah":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pics.me.me/terserah-lodeh-31686397.png',
     preview_image_url='https://pics.me.me/terserah-lodeh-31686397.png'
     ))
     #14
-    if text=="/pun-mager" or data[0]=="mager":
+    if text=="/pun-mager" or data[0]=="mager" or data[1]=="mager":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://storage.jualo.com/original/12555347/bengkel-las-aneka-kar-alat-musik-lainnya-12555347.jpg',
     preview_image_url='https://storage.jualo.com/original/12555347/bengkel-las-aneka-kar-alat-musik-lainnya-12555347.jpg'
     ))
     #15
-    if text=="/pun-pinter" or data[0]=="pinter" or data[0]=="pintar" or data[0]=="smart":
+    if text=="/pun-pinter" or data[0]=="pinter" or data[0]=="pintar" or data[0]=="smart" or data[1]=="pinter" or data[1]=="pintar" or data[1]=="smart":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DkNtokXVAAA3gdr.jpg',
     preview_image_url='https://pbs.twimg.com/media/DkNtokXVAAA3gdr.jpg'
     ))
     #16
-    if text=="/pun-sabi" or data[0]=="sabi" or data[0]=="bisa":
+    if text=="/pun-sabi" or data[0]=="sabi" or data[0]=="bisa" or data[1]=="sabi" or data[1]=="bisa":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DbI1oKdUMAAKHl3.jpg',
     preview_image_url='https://pbs.twimg.com/media/DbI1oKdUMAAKHl3.jpg'
     ))
     #17
-    if text=="/pun-sekip" or data[0]=="skip" or data[0]=="sekip" or data[0]=="sqip" or data[0]=="sqiv":
+    if text=="/pun-sekip" or data[0]=="skip" or data[0]=="sekip" or data[0]=="sqip" or data[0]=="sqiv" or data[1]=="skip" or data[1]=="sekip" or data[1]=="sqip" or data[1]=="sqiv":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://4.bp.blogspot.com/-TXgBZ19sxHs/Wr3bhYlQy6I/AAAAAAAABPs/vbC_Be9GFmsWrhytSZXd90D9DaQymyhdQCLcBGAs/s1600/IMG_20180327_222132.jpg',
     preview_image_url='https://4.bp.blogspot.com/-TXgBZ19sxHs/Wr3bhYlQy6I/AAAAAAAABPs/vbC_Be9GFmsWrhytSZXd90D9DaQymyhdQCLcBGAs/s1600/IMG_20180327_222132.jpg'
     ))
     #18
-    if text=="/pun-siyap" or data[0]=="siap" or data[0]=="siyap" or data[0]=="shap" or data[0]=="ready" or data[0]=="siaap":
+    if text=="/pun-siyap" or data[0]=="siap" or data[0]=="siyap" or data[0]=="shap" or data[0]=="ready" or data[0]=="siaap" or data[1]=="siap" or data[1]=="siyap" or data[1]=="shap" or data[1]=="ready" or data[1]=="siaap":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://www.teknosaurus.com/wp-content/uploads/2018/03/sayap-e1521330529574.jpg',
     preview_image_url='https://www.teknosaurus.com/wp-content/uploads/2018/03/sayap-e1521330529574.jpg'
     ))
     #20
-    if text=="/pun-thanks" or data[0]=="thanks" or data[0]=="thank" or data[0]=="makasih" or data[0]=="trims":
+    if text=="/pun-thanks" or data[0]=="thanks" or data[0]=="thank" or data[0]=="makasih" or data[0]=="trims" or data[1]=="thanks" or data[1]=="thank" or data[1]=="makasih" or data[1]=="trims":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://3.bp.blogspot.com/-2bHJrd2yl7s/Wr3Sy2zDudI/AAAAAAAABNk/DKwqkIkvufUteDl_CQlvfV98EjDNeTJagCLcBGAs/s1600/IMG_20180327_221555.jpg',
     preview_image_url='https://3.bp.blogspot.com/-2bHJrd2yl7s/Wr3Sy2zDudI/AAAAAAAABNk/DKwqkIkvufUteDl_CQlvfV98EjDNeTJagCLcBGAs/s1600/IMG_20180327_221555.jpg'
     ))
     #21
-    if text=="/pun-kampret" or data[0]=="kampret":
+    if text=="/pun-kampret" or data[0]=="kampret" or data[1]=="kampret":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://cdn2.boombastis.com/wp-content/uploads/2018/03/7kampret.jpeg',
     preview_image_url='https://cdn2.boombastis.com/wp-content/uploads/2018/03/7kampret.jpeg '
@@ -372,55 +382,49 @@ def handle_message(event):
         kamus="Meme Wikihow"
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text=kamus))
     #1
-    if text=="/wkh-terciduk":
+    if text=="/wkh-terciduk" or data[0]=="terciduk" or data[0]=="tercyduk" or data[0]=="tercyduck" or data[0]=="diciduk" or data[0]=="dicyduk" or data[0]=="dicyduck"  or data[1]=="terciduk" or data[1]=="tercyduk" or data[1]=="tercyduck" or data[1]=="diciduk" or data[1]=="dicyduk" or data[1]=="dicyduck":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DUnGmbTVAAYbI8w.jpg',
     preview_image_url='https://pbs.twimg.com/media/DUnGmbTVAAYbI8w.jpg'
     ))
     #2
-    if text=="/wkh-berkelahi1":
+    if text=="/wkh-berkelahi1" or data[0]=="berkelahi" or data[0]=="tengkar" or data[1]=="berkelahi" or data[1]=="tengkar":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://ktawa.com/wp-content/uploads/2018/03/DXvYNzGUMAAc6wT.jpg',
     preview_image_url='https://ktawa.com/wp-content/uploads/2018/03/DXvYNzGUMAAc6wT.jpg'
     ))
-    #2
-    if text=="/wkh-berkelahi2":
-        line_bot_api.reply_message(event.reply_token,ImageSendMessage(
-    original_content_url='https://pics.me.me/thumb_ayo-berkelahi-aku-gasuka-kalian-rukun-tribunstyle-com-meme-wikihow-35295086.png',
-    preview_image_url='https://pics.me.me/thumb_ayo-berkelahi-aku-gasuka-kalian-rukun-tribunstyle-com-meme-wikihow-35295086.png'
-    ))
     #3
-    if text=="/wkh-tabok":
+    if text=="/wkh-tabok" or data[0]=="tabok" or data[1]=="tabok":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://s.kaskus.id/images/2018/03/12/7034635_201803120552320355.jpg',
     preview_image_url='https://s.kaskus.id/images/2018/03/12/7034635_201803120552320355.jpg'
     ))
     #4
-    if text=="/wkh-kalem":
+    if text=="/wkh-kalem" or data[0]=="kalem" or data[1]=="kalem" or data[0]=="calm" or data[1]=="calm":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://s.kaskus.id/images/2018/03/12/7034635_201803120524290806.jpg',
     preview_image_url='https://s.kaskus.id/images/2018/03/12/7034635_201803120524290806.jpg'
     ))
     #5
-    if text=="/wkh-maksiat":
+    if text=="/wkh-maksiat" or data[0]=="maksiat" or data[1]=="maksiat":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DUnCIAUUMAArpyA.jpg',
     preview_image_url='https://pbs.twimg.com/media/DUnCIAUUMAArpyA.jpg'
     ))
     #6
-    if text=="/wkh-bacot":
+    if text=="/wkh-bacot" or data[0]=="bacot" or data[1]=="bacot" or data[0]=="bct" or data[1]=="bct" or data[0]=="bacod" or data[1]=="bacod" or data[0]=="bcd" or data[1]=="bcd":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://s.kaskus.id/images/2018/03/12/7034635_201803120533550838.jpg',
     preview_image_url='https://s.kaskus.id/images/2018/03/12/7034635_201803120533550838.jpg'
     ))
     #7
-    if text=="/wkh-sabar":
+    if text=="/wkh-sabar" or data[0]=="sabar" or data[1]=="sabar":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://i.pinimg.com/750x/f3/83/c3/f383c3106936a4728d924dd7ff945c20.jpg',
     preview_image_url='https://i.pinimg.com/750x/f3/83/c3/f383c3106936a4728d924dd7ff945c20.jpg'
     ))
     #8
-    if text=="/wkh-posthink":
+    if text=="/wkh-posthink" or data[0]=="positif" or data[1]=="positif" or text=="positive thinking":
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
     original_content_url='https://pbs.twimg.com/media/DU3n1OuV4AE9sZg.jpg',
     preview_image_url='https://pbs.twimg.com/media/DU3n1OuV4AE9sZg.jpg'
