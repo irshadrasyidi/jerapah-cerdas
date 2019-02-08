b61aeaf50b8feb2fcd2488332f3aa7a92a934237
irshadrasyidi <didot.brodot@yahoo.com>
2019-02-08 10:07:31 +0700

    satu2

--- app.py
+++ app.py
@@ -64,7 +64,19 @@ def handle_message(event):
     gid = event.source.sender_id
     profile = line_bot_api.get_profile(sender)
 
-    
+    if len(data)>=3:
+        #BODOH
+        #WIKIHOW 9, WIKIHOW 10
+        if data[0]=="bodoh" or data[1]=="bodoh" or data[2]=="bodoh" or data[0]=="bodo" or data[1]=="bodo" or data[2]=="bodo":
+            a = random.randint(0, 1)
+            b = ["https://ktawa.com/wp-content/uploads/2018/05/2761997_201803120735020920.jpg", "https://ktawa.com/wp-content/uploads/2018/09/DXgqnNRVwAA-_6F.jpg"]
+            line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url=b[a],preview_image_url=b[a]))
+        #BAWEL
+        #ANJING 8, PUN 2
+        elif data[0]=="bawel" or data[1]=="bawel" or data[2]=="bawel":
+            a = random.randint(0, 1)
+            b = ["https://s.kaskus.id/images/2018/03/14/7034635_201803140545070870.jpg", "https://1.bp.blogspot.com/-AHXu8nVDgww/Wr3YdzZvCVI/AAAAAAAABPg/AO7Mnycr_oQx3bdzboe6qyW8WyqjynfXACLcBGAs/s1600/IMG_20180327_222109.jpg"]
+            line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url=b[a],preview_image_url=b[a]))
 
     if text=="/menu":
         line_bot_api.reply_message(event.reply_token,TemplateSendMessage(
@@ -160,19 +172,7 @@ def handle_message(event):
         ))
 
 #MULT CHOICE
-    if len(data)>=3:
-        #BODOH
-        #WIKIHOW 9, WIKIHOW 10
-        if data[0]=="bodoh" or data[1]=="bodoh" or data[2]=="bodoh" or data[0]=="bodo" or data[1]=="bodo" or data[2]=="bodo":
-            a = random.randint(0, 1)
-            b = ["https://ktawa.com/wp-content/uploads/2018/05/2761997_201803120735020920.jpg", "https://ktawa.com/wp-content/uploads/2018/09/DXgqnNRVwAA-_6F.jpg"]
-            line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url=b[a],preview_image_url=b[a]))
-        #BAWEL
-        #ANJING 8, PUN 2
-        elif data[0]=="bawel" or data[1]=="bawel" or data[2]=="bawel":
-            a = random.randint(0, 1)
-            b = ["https://s.kaskus.id/images/2018/03/14/7034635_201803140545070870.jpg", "https://1.bp.blogspot.com/-AHXu8nVDgww/Wr3YdzZvCVI/AAAAAAAABPg/AO7Mnycr_oQx3bdzboe6qyW8WyqjynfXACLcBGAs/s1600/IMG_20180327_222109.jpg"]
-            line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url=b[a],preview_image_url=b[a]))
+    
 
 #ANJING
     if text=="meme anjing":
